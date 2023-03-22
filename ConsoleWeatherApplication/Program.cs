28f1a5a477f1b082d26e3306fd4f2397ca19d0d2
Anton <travel>
2023-03-22 19:38:06 +0300

    NewYork

--- ConsoleWeatherApplication/Program.cs
+++ ConsoleWeatherApplication/Program.cs
@@ -4,7 +4,7 @@ WeatherParser parser = new("f7612eba248c0e5548f0bd54c8c008b1");
 
 var Minsk = parser.GetWeather("Minsk");
 var London = parser.GetWeather("London");
-var NewYork = parser.GetWeather("New York22");
+var NewYork = parser.GetWeather("New York");
 
 var results = await Task.WhenAll(new[]{Minsk,London,NewYork});
 
