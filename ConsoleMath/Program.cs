95d5ba3ac48f2e15a81e8197c5bb6a58ef769d76
Anton <travel>
2023-03-23 16:34:08 +0300

    Math recursion

--- ConsoleMath/Program.cs
+++ ConsoleMath/Program.cs
@@ -1,36 +1,67 @@
 ﻿Console.WriteLine("Input expression:");
 var expression = Console.ReadLine().ToUpper();
+string[] items = expression.Split(new[] {'(', ')',',',' '}, StringSplitOptions.RemoveEmptyEntries);
 
-try
+Console.WriteLine($"Recursion result: {CalculateInRecursion()}");
+CalculateInLoop();
+
+double CalculateInRecursion(int i=0)
 {
-    Stack<double> values = new();
-    string[] items = expression.Split(new[] {'(', ')',','});
-    for(int i = items.Length-1; i >= 0; i--)
+    if(double.TryParse(items[i],out double num))
     {
-        if(double.TryParse(items[i],out double num))
-        {
-            values.Push(num);
-        }
-        else if(items[i] == "SUM")
-        {
-            values.Push(values.Pop() + values.Pop());
-        }
-        else if(items[i] == "MULTIPLY")
-        {
-            values.Push(values.Pop() * values.Pop());
-        }
-        else if(items[i] == "POWER")
-        {
-            values.Push(Math.Pow(values.Pop(), values.Pop()));
-        }
-        else if(items[i] == "NEGATIVE")
-        {
-            values.Push(-values.Pop());
-        }
+        return num;
     }
-    Console.WriteLine($"Result: {values.Pop()}");
+    else if(items[i] == "SUM")
+    {
+        return CalculateInRecursion(i+1) + CalculateInRecursion(i+2);
+    }
+    else if(items[i] == "MULTIPLY")
+    {
+        return CalculateInRecursion(i+1) * CalculateInRecursion(i+2);
+    }
+    else if(items[i] == "POWER")
+    {
+        return Math.Pow(CalculateInRecursion(i+1), CalculateInRecursion(i+2));
+    }
+    else if(items[i] == "NEGATIVE")
+    {
+        return -CalculateInRecursion(i+1);
+    }
+    return 0;
 }
-catch
+
+void CalculateInLoop()
 {
-    Console.WriteLine("Invalid expression.");
+    try
+    {
+        Stack<double> values = new();
+        for(int i = items.Length-1; i >= 0; i--)
+        {
+            if(double.TryParse(items[i],out double num))
+            {
+                values.Push(num);
+            }
+            else if(items[i] == "SUM")
+            {
+                values.Push(values.Pop() + values.Pop());
+            }
+            else if(items[i] == "MULTIPLY")
+            {
+                values.Push(values.Pop() * values.Pop());
+            }
+            else if(items[i] == "POWER")
+            {
+                values.Push(Math.Pow(values.Pop(), values.Pop()));
+            }
+            else if(items[i] == "NEGATIVE")
+            {
+                values.Push(-values.Pop());
+            }
+        }
+        Console.WriteLine($"Loop result: {values.Pop()}");
+    }
+    catch
+    {
+        Console.WriteLine("Invalid expression.");
+    }
 }
